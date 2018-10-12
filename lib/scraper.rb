44514c7d2c9abb8ef15e3073b280531485d2073e
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-12 11:04:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    binding.pry
+    #doc.css()
   end
 
   def self.scrape_profile_page(profile_url)
